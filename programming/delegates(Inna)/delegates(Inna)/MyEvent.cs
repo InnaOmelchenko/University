7c5c1a5ef3bb8920eabf7f2faa04944cc67409c1
Inna Omelchenko <33749489+InnaOmelchenko@users.noreply.github.com>
2018-06-03 00:36:03 +0300

    Add files via upload

---
+++ programming/delegates(Inna)/delegates(Inna)/MyEvent.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace delegates_Inna_
+{
+    delegate void MyEventHandler();
+    class MyEvent
+    {
+        public event MyEventHandler cardStuck;
+        public event MyEventHandler cardLost;
+
+        public void callService()
+        {
+            Console.WriteLine("What is your problem?\n" + 
+                "1 - I forgot my credit card in ATM\n" + 
+                "2 - I lost my credit card\n");
+
+            int ans = Convert.ToInt32(Console.ReadLine());
+            if (ans == 1) cardStuck();
+            else if (ans == 2) cardLost();
+            else Console.WriteLine("invalid answer");
+        }
+    }
+}
---
+++ programming/delegates(Inna)/delegates(Inna)/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace delegates_Inna_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyEvent myevent = new MyEvent();
+            myevent.cardStuck += new MyEventHandler(cardStuckHandler);
+            myevent.cardLost += new MyEventHandler(cardLostHandler);
+
+            myevent.callService();
+
+            Console.ReadLine();
+        }
+
+        static void cardStuckHandler()
+        {
+            Console.WriteLine("you will get your new card in the next 10 days");
+        }
+
+        static void cardLostHandler()
+        {
+            Console.WriteLine("you will get your new card in the next 3 weeks\n" + 
+                "and there is no more money on your card)))");
+        }
+    }
+}
